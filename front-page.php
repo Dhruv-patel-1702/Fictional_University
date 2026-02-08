0d0e59b0b03fb21336010371d720e2e69b3c0085
Dhruv Patel <dhruv-patel-170>
2026-02-08 13:30:26 +0530

    New added

--- front-page.php
+++ front-page.php
@@ -51,7 +51,6 @@
           </div>
               <?php }
             ?>
-        
 
           <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link("event"); ?>" class="btn btn--blue">View All Events</a></p>
         </div>
