08f9e0cc7093f0f231b604241e289b158b61425a
Dhruv Patel <dhruv-patel-170>
2026-03-26 22:30:20 +0530

    cleaner code and reducing code

--- archive-program.php
+++ archive-program.php
@@ -1,16 +1,13 @@
 <?php
-  get_header();?>
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">
-            All Programs
-        </h1>
-        <div class="page-banner__intro">
-          <p> There is something for everyone. have look around. </p>
-        </div>
-      </div>
-    </div>
+  get_header();
+         pageBanner(
+          array(
+            'title'=> 'All Programs',
+            'subtitle' => 'See what we have to offer',
+          )
+         );
+  ?>
+    
     <div class="container container--narrow page-section">
          <ul class="link-list min-list" >
             <?php 
